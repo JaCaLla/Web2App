27d3badfb5d978647e40317e74faf93fd885f017
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2025-01-02 18:39:42 +0100

    Initial Commit

---
+++ Web2App/Web2AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Web2AppApp.swift
+//  Web2App
+//
+//  Created by Javier Calatrava on 2/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Web2AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
